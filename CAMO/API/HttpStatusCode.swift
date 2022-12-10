dfff6828b35d04aedb1a36b90728b30e4f2df24b
woopaca <jcw001031@gmail.com>
2022-12-10 20:55:17 +0900

    multipartFormData upload

---
+++ CAMO/API/HttpStatusCode.swift
@@ -0,0 +1,12 @@
+//
+//  HttpStatusCode.swift
+//  CAMO
+//
+//  Created by 지찬우 on 2022/12/10.
+//
+
+import Foundation
+
+struct HttpStatusCode{
+    var rawValue : 
+}
--- CAMO/API/ImageController.swift
+++ CAMO/API/ImageController.swift
@@ -6,5 +6,27 @@
 //
 
 import Foundation
+import Alamofire
 
-
+class ImageController : ObservableObject {
+    
+    func uploadFile(cafeId : String, imageData : Data) {
+        let url = host + "/file/upload/\(cafeId)"
+        
+        AF.upload(multipartFormData: { multipartFormData in
+            multipartFormData.append(imageData, withName: "file")
+        }, to: url, method: .post).response { response in
+            switch response.result {
+            case .success(let value):
+                print("호출 성공 uploadFile")
+                print(value)
+                
+            case .failure(_):
+                print("호출 실패 uploadFile")
+                print(response.result)
+            }
+        }
+                  
+    }
+    
+}
